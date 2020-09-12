7b7cb8b727e5ed5e364bf02b943fd0886d3d0b3f
mafusuke <mafusukee@gmail.com>
2020-09-12 19:56:52 +0900

    command.htmlを追加

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,45 @@
 $(function(){
+  $('a[href^="#"]').click(function(){
+    var time = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" ? 'html' : href);
+    var distance = target.offset().top;
+    $("html, body").animate({scrollTop:distance}, time, "swing");
+    return false;
+  });
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'right': '0px' //右から0pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'right': '-100px' //右から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+
   $('#open_menu').click(function() {
     $('.phone_menu').toggle(500);
     $('.phone_menu_open').css('display','none');
-  })
+  });
   $('#close_menu').click(function() {
     $('.phone_menu').toggle(500);
     $('.phone_menu_open').css('display','inline');
-  })
+  });
+  $('[id=open_command]').click(function() {
+    $(this).children(".command_description").slideToggle();
+  });
 });
