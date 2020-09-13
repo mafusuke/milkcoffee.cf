fd43985053bee6626e121b9005f05b1616d1b0ab
mafusuke <mafusukee@gmail.com>
2020-09-13 12:11:28 +0900

    link先変更

--- docs/script.js
+++ docs/script.js
@@ -10,24 +10,24 @@ $(function(){
   var appear = false;
   var pagetop = $('#page_top');
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+    if ($(this).scrollTop() > 100) {
       if (appear == false) {
         appear = true;
         pagetop.stop().animate({
-          'right': '0px' //右から0pxの位置に
-        }, 300); //0.3秒かけて現れる
+          'right': '0px'
+        }, 300);
       }
     } else {
       if (appear) {
         appear = false;
         pagetop.stop().animate({
-          'right': '-100px' //右から-50pxの位置に
-        }, 300); //0.3秒かけて隠れる
+          'right': '-100px'
+        }, 300);
       }
     }
   });
   pagetop.click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    $('body, html').animate({ scrollTop: 0 }, 500);
     return false;
   });
 
